2f7ec99358d2ce47e9a9de793c9a176294ed8ca4
katniss <bourdeloux.corinne@orange.fr>
2024-07-26 08:28:37 +0200

    mise en place du systeme de routage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/animaux", "Animaux", "/pages/animaux.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+export const websiteName = "Zoo Arcadia";
